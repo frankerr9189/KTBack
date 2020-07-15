353f601cdbecc745ae8298192da9e9c9706e30bf
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-14 22:49:49 -0400

    git init

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+require('dotenv').config();
+
+//import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require("./routes/user");
+
+//db connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+).then(()=> console.log('DB Connected'))
+
+mongoose.connection.on('error', err=>{
+    console.log(`DB connection error: ${err.message}`)
+});
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+//routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+});
