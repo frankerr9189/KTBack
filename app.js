6a2f7c7de8de4dd0b6fab7b10ba7fffd233a564b
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-07-18 22:11:52 -0400

    u

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 
 //db connection
@@ -40,6 +41,7 @@ app.use(cors());
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
 
 const port = process.env.PORT || 8000;
 
--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {create} = require('../controllers/category');
-const {requireSignin, isAdmin, isAuth} = require('../controllers/auth');
-const {userById} = require('../controllers/user');
+const {
+    create, read, categoryById, list, remove, update
+} = require("../controllers/category");
+const {requireSignin, isAuth, isAdmin} = require("../controllers/auth");
+const {userById} = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
+router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update);
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.get("/categories", list);
 
-router.post('/category/create/:userId', requireSignin, isAdmin, isAuth, create);
-
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
