b33860ccb8368103e8624130cf0323853eaa7354
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-11-09 20:32:40 -0500

    a

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 const cors = require("cors");
-const router = express.Router();
+//const router = express.Router();
 const Order = require('./models/orders');
 const {errorHandler} = require('./helpers/dbErrorHandler');
 
@@ -40,9 +40,9 @@ app.use(bodyParser.json({limit: '150mb', extended: true}));
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
-//app.use(express.json());
+app.use(express.json());
 //app.use(bodyParser.raw());
-//app.use(express.json({limit: '150mb'}));
+app.use(express.json({limit: '150mb'}));
 //app.use(express.urlencoded({limit: '150mb'}));
 //app.use(bodyParser());
 
@@ -62,16 +62,6 @@ const stripe = require('stripe')('sk_test_51HJqHJGl9xwp0fdr6IujPQSYcrQ1lHHj8VrSn
 
 // Find your endpoint's secret in your Dashboard's webhook settings
 const endpointSecret = 'whsec_RFDFqMz4r4NTgm2i91bPF5uFiXzYqVTS';
-  
-// Stripe requires the raw body to construct the event
-// Use JSON parser for all non-webhook routes
-// app.use((req, res, next) => {
-//     if (req.originalUrl === '/webhook') {
-//       next();
-//     } else {
-//       bodyParser.json({limit: '550mb', extended: true})(req, res, next);
-//     }
-//   });
   
   function updatePaidOrder(currentSession) {
     console.log('in update function');
